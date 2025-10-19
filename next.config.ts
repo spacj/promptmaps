7cc3a2783437147ddf0d323710611eda15642c3c
malek <malekalqaisi@gmail.com>
2025-10-19 13:30:18 +0200

    first

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off"
+  }
 };
 
 export default nextConfig;
